247fbd55d9a3440fbb7ff865c1ef542ba5e9c6a9
Kissg22 <103967397+Kissg22@users.noreply.github.com>
2024-07-12 10:01:52 +0200

    Add files via upload

---
+++ dBFunctions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.IO;
+
+namespace Kribo.Class
+{
+    class dBFunctions
+    {
+        public static string ConnectionStringSQLite
+        {
+            get
+            {
+                string database = @"C:\Users\user\Desktop\C#\SQLite\SQLite\Database\Contact.s3db";
+                string connectionString = @"Data Source=" + database;
+                return connectionString;
+            }
+        }
+    }
+}
